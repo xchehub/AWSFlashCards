c0b4af22a77eada46d81b94396c6e0687828a747
xccommit <schosnablue@gmail.com>
2022-01-03 17:49:28 +0800

    add DrawerLayoutActivity

--- app/src/main/java/com/erranddaddy/awsflashcards/DrawerLayoutActivity.java
+++ app/src/main/java/com/erranddaddy/awsflashcards/DrawerLayoutActivity.java
@@ -1,7 +1,29 @@
 package com.erranddaddy.awsflashcards;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public abstract class DrawerLayoutActivity extends Activity {
+    private final static String LOG_TAG = "DrawerActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+    }
+
 
 }
